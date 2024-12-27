9ed9f2b773efbe6fd981a7c7b51f79fe55ef4a76
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-12-27 17:06:10 +0530

    Update Registration.jsx
    changed backend URL

--- Frontend/src/Components/Registration.jsx
+++ Frontend/src/Components/Registration.jsx
@@ -35,7 +35,7 @@ const Registration = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/register",
+        "https://hr-management-backend-fbii.onrender.com/api/register",
         userData
       );
 
