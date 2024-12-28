c57e6b1fcf6f33949be9d6e1dae57733f81b7153
Aditya Singh <singhaditya6271@gmail.com>
2024-12-28 14:49:05 +0530

    backend url changes made

--- Frontend/src/Components/Registration.jsx
+++ Frontend/src/Components/Registration.jsx
@@ -35,7 +35,7 @@ const Registration = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/register",
+        "https://hr-management-4kc7.onrender.com/api/register",
         userData
       );
 
